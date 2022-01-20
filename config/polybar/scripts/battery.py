6803c9746c83f5c541ab2acd3026badf4b8e8b28
jejbuitenhuis <jorambuitenhuis@gmail.com>
2022-01-20 16:22:57 +0100

    Make battery script a bit more readable

--- config/polybar/scripts/battery.py
+++ config/polybar/scripts/battery.py
@@ -4,6 +4,11 @@ Python was screaming at me, so here it is
 
 STATE_CHARGING = "Charging\n"
 
+AMOUNT_GOOD = 75
+AMOUNT_ENOUGH = 50
+AMOUNT_MEDIUM = 25
+AMOUNT_LOW = 10
+
 with open("/sys/class/power_supply/BAT0/capacity", 'r') as file_capacity:
     with open("/sys/class/power_supply/BAT0/status", 'r') as file_state:
         end_string = ""
@@ -13,20 +18,20 @@ with open("/sys/class/power_supply/BAT0/capacity", 'r') as file_capacity:
         if state == STATE_CHARGING:
             end_string += "%{T3}%{F#829399}%{F-}%{T-}"
         else:
-            if capacity < 15:
+            if capacity < AMOUNT_LOW:
                 end_string += "%{T3}%{F#829399}%{F-}%{T-}%{F#f99f9b}"
-            elif capacity < 25:
+            elif capacity < AMOUNT_MEDIUM:
                 end_string += "%{T3}%{F#829399}%{F-}%{T-}"
-            elif capacity < 50:
+            elif capacity < AMOUNT_ENOUGH:
                 end_string += "%{T3}%{F#829399}%{F-}%{T-}"
-            elif capacity < 75:
+            elif capacity < AMOUNT_GOOD:
                 end_string += "%{T3}%{F#829399}%{F-}%{T-}"
             else:
                 end_string += "%{T3}%{F#829399}%{F-}%{T-}"
 
         end_string += ' ' + str(capacity) + '%'
 
-        if state != STATE_CHARGING and capacity < 10:
+        if state != STATE_CHARGING and capacity < AMOUNT_LOW:
             end_string += "%{F-}"
 
         print(end_string)
